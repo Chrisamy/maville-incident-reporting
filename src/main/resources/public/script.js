b0a8acf5038e6410a8e4988bbac759d1c6cde799
MaxGour <max.gour.work@gmail.com>
2025-09-26 11:02:58 -0400

    restructuration du code pour le prototype

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -11,34 +11,8 @@ document.getElementById("hello-world-btn").addEventListener("click", async () =>
     }
 });
 
-function addRequestButton() {
-    var table = document.getElementById("table-request");
-    let addreseProb = prompt("C'est ou le probleme?")
-    let typeProb = prompt("C'est quelle type de probleme?")
-    let Status = prompt("mettre le status (pour raison de teste seulement)")
-
-    var row = table.insertRow(-1);
-    var cellRequestNum = row.insertCell(0);
-    var cellAddProb = row.insertCell(1);
-    var cellTypeProb = row.insertCell(2);
-    var cellStatus = row.insertCell(3);
-
-    cellRequestNum.innerHTML = requestNum;
-    cellAddProb.innerHTML = addreseProb;
-    cellTypeProb.innerHTML = typeProb;
-    cellStatus.innerHTML = Status;
-
-    requestNum += 1;
-
-    console.log(colorSwap)
-
-    if (colorSwap == 1) {
-        row.setAttribute('id', 'request-row-blue');
-        colorSwap *= -1;
-    } else {
-        row.setAttribute('id', 'request-row-green')
-        colorSwap *= -1;
-    }
+async function addRequestButton() {
+    const response = await fetch('/api/request_button')
 
 
 }
