ad29cbc3a83ba068c7907d8634bd92b72a81ab05
MaxGour <max.gour.work@gmail.com>
2025-10-03 09:40:13 -0400

    creer jar et auto open tab

--- src/main/java/ca/udem/maville/App.java
+++ src/main/java/ca/udem/maville/App.java
@@ -1,6 +1,9 @@
 package ca.udem.maville;
 
 import io.javalin.Javalin;
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +14,12 @@ public class App {
             config.staticFiles.add("/public");
         }).start(7000);
 
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.browse(new URI("http://localhost:7000"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         app.get("/api/hello_world", context -> {
             Map<String, String> response = new HashMap<>();
