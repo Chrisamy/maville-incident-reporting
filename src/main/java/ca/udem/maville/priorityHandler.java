465ae972ac28e46b7089edddb2ea3cb278c680fc
MaxPengu <max.gour.work@gmail.com>
2025-11-12 16:17:53 -0500

    Added Remaining Classes
    Classes pour prestataires et Agents sont ajouters

---
+++ src/main/java/ca/udem/maville/priorityHandler.java
@@ -0,0 +1,11 @@
+package ca.udem.maville;
+
+public class priorityHandler {
+
+    private FormResident getProblem(String id){
+        return null;
+    }
+
+    private void AssignPriority(String id, String Priority){
+    }
+}
