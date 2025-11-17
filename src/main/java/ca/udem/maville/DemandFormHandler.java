3b25953d21e31e5a73a288845200e45731b610fa
Adealeus <phil.racz@gmail.com>
2025-11-16 22:12:58 -0500

    update demandForm, demandFormhandler and demands..json

--- src/main/java/ca/udem/maville/DemandFormHandler.java
+++ src/main/java/ca/udem/maville/DemandFormHandler.java
@@ -3,10 +3,10 @@ package ca.udem.maville;
 public class DemandFormHandler {
 
 
-    public void submitDemand(Prestataire prestataire, String titreProjet, EnumWorkType workType,
-                             String location, String description, String dateDebut, String dateFin, String id) {
-        DemandForm form = new DemandForm(prestataire, titreProjet, workType, location,
-                description, dateDebut, dateFin, id);
+    public void submitDemand(Prestataire prestataire, int companyNumber, String projectTitle, EnumWorkType workType,
+                             String location, String description, String startDate, String endDate, String id, float costEstimate) {
+        DemandForm form = new DemandForm(prestataire, companyNumber, projectTitle, workType, location,
+                description, startDate, endDate, id, costEstimate);
         DemandRepository.addForm(form);
     }
 
