52d8b5eac95627879dfe5b2e958d4209931b480e
Al3ksy <alexislabelle04@gmail.com>
2025-12-18 21:53:52 -0500

    test release auto

--- src/main/java/ca/udem/maville/User.java
+++ src/main/java/ca/udem/maville/User.java
@@ -3,8 +3,7 @@ package ca.udem.maville;
 public class User {
     private String username;
     private String password;
-
-
+    
     public User() {}
 
     public User(String username, String password) {
